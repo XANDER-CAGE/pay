77bc5ee0d1e892f3d7df79577aa90e65ffcf118f
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-03-26 03:53:38 +0500

    rewrite xml

--- src/modules/processing/humoProcessing.service.ts
+++ src/modules/processing/humoProcessing.service.ts
@@ -283,11 +283,16 @@ export class HumoProcessingService {
         password: this.humoSoapPassword,
       },
     });
+    console.log('jsonData.data: ', jsonData.data);
     const jsonfromXml = await parser.toJson(jsonData.data);
-    const json =
-      jsonfromXml['SOAP-ENV:Envelope']['SOAP-ENV:Body'][
-        'ebppif1:PaymentResponse'
-      ];
+    console.log('jsonfromXml: ', jsonfromXml);
+    console.log(
+      'jsonfromXml SOAP-ENV:Envelope: ',
+      jsonfromXml['SOAP-ENV:Envelope'],
+    );
+    console.log('jsonfromXml Envelope: ', jsonfromXml['Envelope']);
+
+    const json = jsonfromXml['Envelope']['Body']['ebppif1:PaymentResponse'];
     const paymentID = json['paymentID'];
     const paymentRef = json['paymentRef'];
     const action = json['action'];
