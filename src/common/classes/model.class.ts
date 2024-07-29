c77dde4c0a664ea068f8882bad60aa596852f934
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-07-29 18:12:43 +0500

    fix dateeeeeeeeeeeeee

--- src/common/classes/model.class.ts
+++ src/common/classes/model.class.ts
@@ -214,8 +214,7 @@ export class CoreApiResponse {
       AccountId: data.AccountId || null,
       Description: data.Description || null,
       CreatedDate: `/Date(${date.getTime()})/` || null,
-      CreatedDateIso:
-        date?.toISOString()?.replace('T', ' ')?.replace('Z', '') || null,
+      CreatedDateIso: date?.toISOString()?.replace('Z', '') || null,
       IpAddress: data.IpAddress || null,
       IpCountry: data.IpCountry || null,
       IpCity: data.IpCity || null,
