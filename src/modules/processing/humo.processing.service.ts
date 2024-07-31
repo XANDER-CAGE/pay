aa46f9eaa4b003840523c6e11ac1ff5a5817b501
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-08-01 00:23:41 +0500

    fix: void xml

--- src/modules/processing/humo.processing.service.ts
+++ src/modules/processing/humo.processing.service.ts
@@ -550,7 +550,7 @@ export class HumoProcessingService {
     <soapenv:Header/>
     <soapenv:Body>
     <urn:CancelRequest>
-    <paymentRef>${transaction.processing_ref_num}</paymentRef>>
+    <paymentRef>${transaction.processing_ref_num}</paymentRef>
     <paymentOriginator>${this.humoSoapUsername}</paymentOriginator>
     </urn:CancelRequest>
     </soapenv:Body>
