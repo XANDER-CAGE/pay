3d7eb2be488df428c4d7b18a73f9e5679d9903cb
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-09-30 17:15:40 +0500

    fix: auth methods status 200

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -73,7 +73,7 @@ export class PaymentsController {
   }
 
   @UseGuards(AuthGuard)
-  @HttpCode(201)
+  @HttpCode(200)
   @Post('cards/auth')
   async cardsAuth(@Body() dto: CardsHoldDto, @Req() req: MyReq) {
     return await this.paymentsService.cardsAuth({
@@ -88,7 +88,7 @@ export class PaymentsController {
   }
 
   @UseGuards(AuthGuard)
-  @HttpCode(201)
+  @HttpCode(200)
   @Post('tokens/auth')
   async tokensAuth(@Body() dto: TokensHoldDto, @Req() req: MyReq) {
     return await this.paymentsService.tokensAuth({
