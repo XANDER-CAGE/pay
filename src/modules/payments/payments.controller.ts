3ef384807475f5faa5d13bf51bbfde816facc387
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-10-07 17:39:12 +0500

    fix: status code auth methods

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -84,7 +84,7 @@ export class PaymentsController {
   }
 
   @UseGuards(AuthGuard)
-  @HttpCode(500)
+  @HttpCode(200)
   @Post('cards/auth')
   async cardsAuth(@Body() dto: CardsHoldDto, @Req() req: MyReq) {
     return await this.paymentsService.cardsAuth({
@@ -99,7 +99,7 @@ export class PaymentsController {
   }
 
   @UseGuards(AuthGuard)
-  @HttpCode(500)
+  @HttpCode(200)
   @Post('tokens/auth')
   async tokensAuth(@Body() dto: TokensHoldDto, @Req() req: MyReq) {
     return await this.paymentsService.tokensAuth({
